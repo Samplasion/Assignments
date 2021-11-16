3211405786af2c40c84a70e7b61ef540b8872a63
Francesco <samplasion@gmail.com>
2021-11-16 11:35:25 +0100

    Update interactive runner and use yaml-based info files

--- 1/run.sh
+++ 1/run.sh
@@ -1,11 +1,3 @@
-echo ""
-echo "=============================================================="
-echo "=                                                            ="
-echo "=                      Exercise #1                           ="
-echo "=                                                            ="
-echo "=============================================================="
-echo ""
-
 g++ -std=c++11 -Wall -Wextra -pedantic -Werror -o main.o main.cpp
 
 ./main.o
--- run.py
+++ run.py
@@ -1,11 +1,24 @@
 #! /usr/bin/env python3
 
 import os
+import oyaml as yaml
 
 exercise = input("Which exercise would you like to run? ")
+print("")
 
 # Check if the directory exists
 if os.path.isdir(exercise):
+    # Check if the info file exists
+    if os.path.isfile(exercise + "/info.yaml"):
+        # Load the info file
+        with open(exercise + "/info.yaml", "r") as info_file:
+            info = yaml.safe_load(info_file)
+
+            # Print info
+            print("Exercise #%s\n" % exercise)
+            print("Assigned on %s" % info["assigned"].strftime("%B %d, %Y"))
+            print("\n\n%s" % info["description"])
+    # Run exercise
     os.chdir(exercise)
     os.system("sh run.sh")
 else:
